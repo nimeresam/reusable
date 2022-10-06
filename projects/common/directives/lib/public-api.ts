bd3d0918e9055a3eebdf6cf3e77f3843da90f109
nimeresam <nimeresam95@gmail.com>
2022-10-06 17:11:59 +0300

    fix exported directivesModule

--- projects/common/directives/lib/public-api.ts
+++ projects/common/directives/lib/public-api.ts
@@ -1,3 +1,6 @@
 export * from './copy-to-clipboard.directive';
 export * from './paste-from-clipboard.directive';
 export * from './confirm.directive';
+export * from './directives.module';
+export * from './right-click.directive';
+export * from './middle-click.directive';
